cd5e05c78462a0d38013096e627ee7e3b92b0923
Loic M <loic@Loics-Mac.local>
2020-01-30 23:10:59 -0800

    change logo and iphone images

--- sell/views.py
+++ sell/views.py
@@ -5,12 +5,12 @@ def home(request):
     trip = Trip.objects.all()
     print(trip[0].trip_link)
     customers = Customer.objects.all()
-    return render(request, 'home.html', {'customers': customers, 'trip':trip[0].trip_link})
+    return render(request, 'home.html', {'customers': customers, 'trip':trip[0].trip_link, 'title': 'Product Surveys'})
 
 
 def policy(request):
     policys = Policy.objects.all()
-    return render(request, 'policy.html', {'policy': policys[0].content})
+    return render(request, 'policy.html', {'policy': policys[0].content, 'title': 'Policy'})
 def terms(request):
     term = Term.objects.all()
-    return render(request, 'terms.html', {'term': term[0].content})
+    return render(request, 'terms.html', {'term': term[0].content, 'title': 'Terms'})
